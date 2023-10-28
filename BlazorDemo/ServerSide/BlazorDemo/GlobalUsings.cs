86662cc68e80aecb404cf1924376ea53be8efff3
DaveB <davebrighton@gmail.com>
2023-10-28 10:23:20 -0400

    added both server and wsam exaple with fluxor

---
+++ BlazorDemo/ServerSide/BlazorDemo/GlobalUsings.cs
@@ -0,0 +1,8 @@
+// Global using directives
+
+global using BingoFeatureModule.Store;
+global using BlazorDemo.Data;
+global using Fluxor;
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Web;
+global using ScrumPokerFeatureModule.Store;
--- BlazorDemo/ServerSide/BlazorDemo/Program.cs
+++ BlazorDemo/ServerSide/BlazorDemo/Program.cs
@@ -1,13 +1,25 @@
-using BlazorDemo.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+
+builder.Services.AddFluxor(opt =>
+{
+#if DEBUG
+    opt.ScanAssemblies(
+            typeof(BingoState).Assembly,
+            typeof(PokerState).Assembly)
+        .UseReduxDevTools();
+#else
+    opt.ScanAssemblies(
+        typeof(BingoState).Assembly,
+        typeof(PokerState).Assembly);
+#endif
+});
+
 
 var app = builder.Build();
 
