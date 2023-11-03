4555dc769cf595dac5da693fb19e332888368035
David Brighton <davebrighton@gmail.com>
2023-11-03 17:18:55 -0400

    wip

--- FeatureModules/FluxorChess/Models/ChessPeace.cs
+++ FeatureModules/FluxorChess/Models/ChessPeace.cs
@@ -1,4 +1,6 @@
-﻿namespace FluxorChess.Models;
+﻿// Ignore Spelling: Fluxor
+
+namespace FluxorChess.Models;
 
 public enum ChessPieceType
 {
@@ -16,6 +18,26 @@ public class ChessPiece
     public int X { get; set; }
     public char Y { get; set; }
     public bool IsWhite { get; set; }
-    public bool IsAlive { get; set; } = true;
-    public ChessPieceType PieceType { get; set; }
+
+    private ChessPieceType _pieceType;
+    public ChessPieceType PieceType
+    {
+        get => _pieceType;
+        set
+        {
+            _pieceType = value;
+            UpdateImageSrc();
+        }
+    }
+
+    public string ImageSrc { get; private set; }
+
+    private void UpdateImageSrc()
+    {
+        var basePath = "./wwwroot/images/";
+        var color = IsWhite ? "white" : "black";
+        var fileName = $"{color}_{_pieceType.ToString().ToLower()}.svg";
+        Console.WriteLine(fileName);
+        ImageSrc = $"{basePath}{fileName}";
+    }
 }
